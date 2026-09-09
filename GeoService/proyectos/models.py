d334accccbfcdb7b97e0e44d60e4c1c15036bd83
Sebastian Cabrera <cabrerasebastian867@gmail.com>
2026-09-09 15:42:15 -0300

    Avances co el backend

--- GeoService/proyectos/models.py
+++ GeoService/proyectos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class proyecto(models.Model):
+
+    ESTADOS = [
+        ('ejecucion', 'En ejecución'),
+        ('progreso', 'En progreso'),
+        ('terminado', 'Terminado'),
+        ('cancelado', 'Cancelado'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    año = models.IntegerField()
+    estado = models.CharField(max_length=50, choices=ESTADOS)
+
+    def __str__(self):
+        return self.nombre
+
